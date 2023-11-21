f55ca887c9ea0ca860854a69e4bf004335ddcac7
Taras <141884340+ntarv@users.noreply.github.com>
2023-11-21 17:02:33 +0300

    Update install_jenkins.sh

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 sudo /bin/bash
-apt update
-apt install default-jdk -y
-mkdir -p /usr/share/keyrings
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
-apt update
-apt install jenkins -y
-systemctl start jenkins
-systemctl enable --now jenkins
+apt-get update -y
+apt-get install fontconfig openjdk-17-jre -y
+wget -O /usr/share/keyrings/jenkins-keyring.asc  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key /
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]   https://pkg.jenkins.io/debian-stable binary/ | sudo tee   /etc/apt/sources.list.d/jenkins.list > /dev/null
+apt-get update -y
+apt-get install jenkins -y
 ufw disable
+sudo jenkins
+sudo cat /root/.jenkins/secrets/initialAdminPassword
